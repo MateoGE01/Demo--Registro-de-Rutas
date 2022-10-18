3c86034d673e2e0eb4ec3066cfba1c90a25cf352
MateoGE01 <mateoguerrero@uninorte.edu.co>
2022-10-17 21:58:14 -0500

    Cambio

---
+++ practicaLabel.py
@@ -0,0 +1,19 @@
+from logging import root
+from tkinter import *
+
+principal = Tk()
+principal.title("SIU")
+
+marcoprincipal = Frame(principal, width=500, height=400)
+marcoprincipal.pack()
+
+miLabel = Label(marcoprincipal, text="Goku", fg="red", font=("Times New Romans", 18))#Permitira poner imagenes o textos 
+miLabel.place(x=250, y=200)#Ubicara "miLabel"
+
+otrolabel = Label(marcoprincipal, text="Manzanita")
+otrolabel.place(x=100, y=250)
+
+miImagen = PhotoImage(file="index.png")#Con esto puedo poner imagenes
+Label(marcoprincipal, image=miImagen).place(x=1, y=1)
+
+principal.mainloop()
