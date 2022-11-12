1a247cacafdb267b7cd8a0f819b8a3b1da302d85
BrayanGuti <bagutierrez@uninorte.edu.co>
2022-11-11 22:13:34 -0500

    brayan

--- code to run.py
+++ code to run.py
@@ -1,18 +1,17 @@
 from Clases import *
 
+
+
+
+
 Ruta_universidad = Ruta()
 
+Ruta_universidad.agregar("Terminal", "10.910700%2C-74.795472")
+Ruta_universidad.agregar("Estadio", "10.923466%2C-74.801403")
+Ruta_universidad.agregar("Caribe verde", "10.956486%2C-74.836018") 
+Ruta_universidad.agregar("Uni atlantico", "11.020045%2C-74.871536")
+Ruta_universidad.tiempos_ideales()
 
-#Ruta_universidad.agregar_estaciones(3)
+print(Ruta_universidad.tiempos[0])
+print(Ruta_universidad.distancias[0])
 
-
-Ruta_universidad.agregar("Murillo soledad")
-Ruta_universidad.agregar("Estadio")
-Ruta_universidad.agregar("Caribe verde") 
-Ruta_universidad.agregar("puente la 38")
-Ruta_universidad.agregar("Alamedade del rio")
-Ruta_universidad.agregar("miramar")
-Ruta_universidad.agregar("corredor universitaro")
-
-bus1 = bus(Ruta_universidad, 1234)
-bus1.calcular_horas(1200)
